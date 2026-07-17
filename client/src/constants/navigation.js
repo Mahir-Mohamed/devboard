d32588ad438db3e8f332e402e87041641dee07ee
Mahir-Mohamed <mahirmohammed01@gmail.com>
2026-07-17 16:21:51 +0530

    Refactor sidebar using dynamic navigation data

---
+++ client/src/constants/navigation.js
@@ -0,0 +1,26 @@
+export const sidebarItems = [
+  {
+    id: 1,
+    title: "Dashboard",
+  },
+  {
+    id: 2,
+    title: "Projects",
+  },
+  {
+    id: 3,
+    title: "Tasks",
+  },
+  {
+    id: 4,
+    title: "Calendar",
+  },
+  {
+    id: 5,
+    title: "Reports",
+  },
+  {
+    id: 6,
+    title: "Settings",
+  },
+];
